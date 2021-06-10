846635a4442380d4460375fca58a31fca55b5261
github username <github email address>
2021-06-09 21:03:04 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 // Code your solutions in this file
-const name = ["Lisa", "Kaitlin", "Jan"];
-const event = 'surprise';
 
-function writeCards(name, event) {
-    let newArray = [];
-    for (let i = 0; i < name.length; i++) {
-        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
-        debugger;
+function writeCards(names, event) {
+    let myArray = []
+    for (let i = 0; i < names.length; i++) {
+        myArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
     }
-    return newArray;
+return myArray
 }
 
-writeCards('name', 'event');
-
-function countDown(init = 10) {
-    while (init >= 0) {
-        console.log(init--);
+function countDown(number) {
+    let loop = number
+    while (loop >= 0) {
+        console.log(loop--);
     }
 }
